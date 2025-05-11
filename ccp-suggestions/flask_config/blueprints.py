95ee8d10b015c41a353457dfa19587e76c0347fd
Juan Pablo Rodriguez <jp.rodriguez22@uniandes.edu.co>
2025-05-10 23:11:20 -0500

    Fix: Añadir llaves de JSON para GCP Cloud Run

--- ccp-suggestions/flask_config/blueprints.py
+++ ccp-suggestions/flask_config/blueprints.py
@@ -11,21 +11,21 @@ def health_check():
 
 @blueprint.post('/predict')
 def predict_model():
-    body = request.get_json()
+    body = request.get_json()['message']['attributes']
     print(body)
     r = PredictionModel(body).execute()
     return jsonify(r['response']), r['status_code']
 
 @blueprint.post('/recommendations')
 def create_recommendations():
-    body = request.get_json()
+    body = request.get_json()['message']['attributes']
     print(body)
     r = GenerateRecommendations(body).execute()
     return jsonify(r['response']), r['status_code']
 
 @blueprint.post('/mail_recommendations')
 def send_recommendations():
-    body = request.get_json()
+    body = request.get_json()['message']['attributes']
     print(body)
     r = SendEmail(body).execute()
     return jsonify(r['response']), r['status_code']
